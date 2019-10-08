d124d1d53c1c4e5f62c28762ba31a1f8c45fa2b1
dbriksza <dbriksza@gmail.com>
2019-10-08 15:43:29 -0400

    first commit

---
+++ src/reducers/reducer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const todos = [
+  {
+    item: "Learn about reducers",
+    completed: false,
+    id: 3892987589
+  },
+  {
+    item: "make a reducer",
+    completed: false,
+    id: 3892987590
+  },
+  {
+    item: "display the reducer",
+    completed: false,
+    id: 3892987591
+  }
+];
+
+export const Reducer = () => {
+  return todos;
+};
